7c2ac3a8fdbfc8df9bc30e9a314fe1afca0f6c81
Michal Novotny <xmnovotny@gmail.com>
2021-06-14 00:59:14 +0200

    Refitting time is calculated only for units, that haven't same item as in the refit task.

---
+++ RefitTaskTimeCorrection.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using VoxelTycoon;
+using VoxelTycoon.Tracks;
+using VoxelTycoon.Tracks.Tasks;
+
+namespace ScheduleStopwatch
+{
+    [HarmonyPatch]
+    class RefitTaskTimeCorrection
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(RefitTaskDelayedAction), "GetStorageToSet")]
+        private static void RefitTaskDelayedAction_GetStorageToSet_pof(RefitTaskDelayedAction __instance, ref Storage __result, VehicleUnit unit)
+        {
+            FileLog.Log("Refit");
+            if (__result != null && unit.Storage != null && __result.Item == unit.Storage.Item)
+            {
+                FileLog.Log("Refit: null");
+                //same strorage = no need to refit
+                __result = null;
+            }
+        }
+    }
+}
